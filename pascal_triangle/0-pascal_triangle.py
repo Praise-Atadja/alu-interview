ab315dde60f37537f7c5aa9989cb1e6923f630b2
Praise-Atadja <p.atadja@alustudent.com>
2023-03-22 15:36:59 +0300

    pascal method

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Pascal triangle method"""
+
+
+def pascal_triangle(n):
+    """Function that returns a list of lists of integers
+        representing the Pascal's triangle of n"""
+    first_list = []
+    if (n <= 0):
+        return first_list
+    for i in range(1, (n + 1)):
+        second_list = []
+        for j in range(i):
+            second_list.append(1)
+        first_list.append(second_list)
+    for i in range(len(first_list)):
+        for j in range(i):
+            if j != 0:
+                first_list[i][j] = first_list[i - 1][j] + first_list[i - 1][j - 1]
+    return first_list
