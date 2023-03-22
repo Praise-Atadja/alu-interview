83d65e1bc7696f5e5847e141e82bc4efc1ec2ff4
Praise-Atadja <p.atadja@alustudent.com>
2023-03-22 15:40:12 +0300

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,8 +3,7 @@
 
 
 def pascal_triangle(n):
-    """Function that returns a list of lists of integers
-        representing the Pascal's triangle of n"""
+    """Function that returns a list of lists of integers"""
     first_list = []
     if (n <= 0):
         return first_list
